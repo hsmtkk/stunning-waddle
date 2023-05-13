80f557c1639eb63a6f44c0b10541d4fca276d0f9
user <user@example.com>
2023-05-13 07:15:12 +0000

    up

--- back/gourmet_search/app.py
+++ back/gourmet_search/app.py
@@ -8,9 +8,8 @@ def lambda_handler(event, context):
     print(f"{event=}")
     print(f"{context=}")
 
-    decoded = json.loads(event)
-    keyword = decoded["keyword"]
-
+    body = json.loads(event["body"])
+    keyword = body["keyword"]
     result = gourmet_search(keyword)
 
     return {
@@ -22,11 +21,20 @@ GOURMET_SEARCH_URL = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/"
 
 def gourmet_search(keyword:str) -> dict:
     api_key = os.environ["GOURMET_SEARCH_API_KEY"]
-    data = {
+    params = {
         "key": api_key,
         "keyword": keyword,
+        "format": "json",
     }
-    resp = requests.post(GOURMET_SEARCH_URL, data=data)
+    resp = requests.get(GOURMET_SEARCH_URL, params=params)
     if resp.status_code < 200 or resp.status_code >= 300:
         raise Exception(f"HTTP {resp.status_code}: {resp.text}")
-    return resp.json()
+    resp_dict = resp.json()
+    results = list()
+    for shop in resp_dict["results"]["shop"]:
+        results.append({
+            "name": shop["name"],
+            "lat": shop["lat"],
+            "lon": shop["lng"],
+        })
+    return results
--- front/front.py
+++ front/front.py
@@ -1,11 +1,21 @@
 import os
+
+import pandas as pd
 import requests
 import streamlit as st
 
 lambda_function_url = os.environ["LAMBDA_FUNCTION_URL"]
-keyword = st.text_input(label="キーワード", value="魚のおいしい店")
-data = {"keyword": keyword}
-resp = requests.post(lambda_function_url, data=data)
-if resp.status_code < 200 or resp.status_code >= 300:
-    raise Exception(f"HTTP {resp.status_code}: {resp.text}")
-st.write(resp.json())
+
+with st.form("gourmet"):
+    keyword = st.text_input(label="キーワード", value="新宿")
+    submitted = st.form_submit_button("送信")
+    if submitted:
+        data = {"keyword": keyword}
+        resp = requests.post(lambda_function_url, json=data)
+        if resp.status_code < 200 or resp.status_code >= 300:
+            raise Exception(f"HTTP {resp.status_code}: {resp.text}")
+        shops = resp.json()
+        # st.write(shops)
+        df = pd.DataFrame.from_dict(shops)
+        st.dataframe(df)
+        st.map(df)
